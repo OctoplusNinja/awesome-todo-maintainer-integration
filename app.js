b47b322f3a9fd46e6b8fc6bc6b0941cdfdc364b2
Aniruddha Das <aniruddha.das.852@gmail.com>
2022-02-12 20:24:22 +0530

    added Create API

--- app.js
+++ app.js
@@ -2,29 +2,90 @@ const { Client } = require("@notionhq/client");
 require("dotenv").config();
 
 const notion = new Client({ auth: process.env.NOTION_KEY });
-const databaseID = process.env.NOTION_DATABASE_ID;
 
 module.exports = {
-    readTodo: async function readTodo() {
-        const response = await notion.databases.query({
-            database_id: databaseID,
-        });
-        let result = response.results.map((res) => {
-            return {
-                pageID: res.id,
-                url: res.url,
-                title: res.properties?.Todo?.title[0]?.plain_text,
-                priority: res.properties?.Priority.select?.name,
-                status: res.properties?.Status?.select?.name,
-                remarks: res.properties?.Remarks?.rich_text[0]?.plain_text,
-            };
-        });
-        return result;
+    readTodo: async function readTodo(databaseID) {
+        try {
+            const response = await notion.databases.query({
+                database_id: databaseID,
+            });
+            let results = response.results.map((res) => {
+                return {
+                    pageID: res.id,
+                    url: res.url,
+                    title: res.properties?.Todo?.title[0]?.plain_text,
+                    priority: res.properties?.Priority.select?.name,
+                    status: res.properties?.Status?.select?.name,
+                    remarks: res.properties?.Remarks?.rich_text[0]?.plain_text,
+                };
+            });
+            return { success: "true", results };
+        }
+        catch (err) {
+            console.log("status Code: " + err.status);
+            return { success: "false" };
+        }
+
     },
-    dev: async function dev() {
-        const response = await notion.databases.query({
-            database_id: databaseID,
-        });
-        return response.results;
+    createTodo: async function createTodo(databaseID, data) {
+        try {
+            const response = await notion.pages.create({
+                parent: {
+                    database_id: databaseID
+                },
+                properties: {
+                    Todo: {
+                        type: "title",
+                        title: [
+                            {
+                                text: {
+                                    content: data.title
+                                }
+                            }
+                        ]
+                    },
+                    Priority:
+                    {
+                        select: {
+                            name: data.priority
+                        }
+                    },
+                    Status:
+                    {
+                        select: {
+                            name: data.status
+                        }
+                    },
+                    Remarks:
+                    {
+                        rich_text: [
+                            {
+                                text: {
+                                    content: data.remarks
+                                }
+                            }
+                        ]
+                    }
+                }
+            });
+            return { success: "true" }
+        }
+        catch (err) {
+            return { success: "false" }
+        }
+
+    },
+    dev: async function dev(databaseID) {
+        try {
+            const response = await notion.databases.query({
+                database_id: databaseID,
+            });
+            // console.log(response)
+            return { success: "true", results: response.results };
+        }
+        catch (err) {
+            return { success: "false" }
+        }
+
     }
 };
--- server.js
+++ server.js
@@ -1,22 +1,27 @@
 require("dotenv").config();
-const { readTodo, dev } = require("./app");
+const { readTodo, createTodo, dev } = require("./app");
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
-const devPORT = 6969;
+
+app.use(express.json())
 
 app.get("/", (req, res) => {
     res.send("Bruhhhh");
 });
 
 app.get("/readTodo", async (req, res) => {
-    const todos = await readTodo();
-    // console.log(todos);
+    const todos = await readTodo(req.query.databaseID);
     res.send(todos);
 });
 
+app.post('/createTodo', async (req, res) => {
+    const result = await createTodo(req.query.databaseID, req.body);
+    res.send(result);
+})
+
 app.get('/dev', async (req, res) => {
-    const result = await dev();
+    const result = await dev(req.query.databaseID);
     res.send(result);
 })
 
