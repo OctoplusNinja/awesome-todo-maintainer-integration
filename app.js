0cca0ba7ac784dd54d1f3b609be93c59bb269d01
lostgirljourney <falgunisarkar526@gmail.com>
2022-04-28 05:41:53 +0530

    Removed unnecessary lines

--- app.js
+++ app.js
@@ -1,158 +1,142 @@
-// const { Client } = require("@notionhq/client");
-require("dotenv").config();
-
-// const notion = new Client({ auth: process.env.NOTION_KEY });
+require('dotenv').config();
 
 module.exports = {
-    status: async function status(notion, databaseID) {
-        try {
-            const response = await notion.databases.retrieve({ database_id: databaseID });
-            return { success: true }
-        }
-        catch (err) {
-            return { success: false }
-        }
-    },
-    readTodo: async function readTodo(notion, databaseID) {
-        try {
-            const response = await notion.databases.query({
-                database_id: databaseID,
-            });
-            let results = response.results.map((res) => {
-                return {
-                    pageID: res.id,
-                    url: res.url,
-                    title: res.properties?.Todo?.title[0]?.plain_text,
-                    priority: res.properties?.Priority.select?.name,
-                    status: res.properties?.Status?.select?.name,
-                    remarks: res.properties?.Remarks?.rich_text[0]?.plain_text,
-                };
-            });
-            return { success: true, results };
-        }
-        catch (err) {
-            console.log("status Code: " + err.status);
-            return { success: false };
-        }
+	status: async function status(notion, databaseID) {
+		try {
+			const response = await notion.databases.retrieve({
+				database_id: databaseID,
+			});
+			return { success: true };
+		} catch (err) {
+			return { success: false };
+		}
+	},
+	readTodo: async function readTodo(notion, databaseID) {
+		try {
+			const response = await notion.databases.query({
+				database_id: databaseID,
+			});
+			let results = response.results.map((res) => {
+				return {
+					pageID: res.id,
+					url: res.url,
+					title: res.properties?.Todo?.title[0]?.plain_text,
+					priority: res.properties?.Priority.select?.name,
+					status: res.properties?.Status?.select?.name,
+					remarks: res.properties?.Remarks?.rich_text[0]?.plain_text,
+				};
+			});
+			return { success: true, results };
+		} catch (err) {
+			console.log('status Code: ' + err.status);
+			return { success: false };
+		}
+	},
+	createTodo: async function createTodo(notion, databaseID, data) {
+		try {
+			const response = await notion.pages.create({
+				parent: {
+					database_id: databaseID,
+				},
+				properties: {
+					Todo: {
+						type: 'title',
+						title: [
+							{
+								text: {
+									content: data.title,
+								},
+							},
+						],
+					},
+					Priority: {
+						select: {
+							name: data.priority,
+						},
+					},
+					Status: {
+						select: {
+							name: data.status,
+						},
+					},
+					Remarks: {
+						rich_text: [
+							{
+								text: {
+									content: data.remarks,
+								},
+							},
+						],
+					},
+				},
+			});
+			return { success: true, pageID: response.id };
+		} catch (err) {
+			return { success: false };
+		}
+	},
+	deleteTodo: async function deleteTodo(notion, pageID) {
+		try {
+			const response = await notion.blocks.delete({
+				block_id: pageID,
+			});
+			console.log(response);
+			return { success: true };
+		} catch (err) {
+			console.log(err);
+			return { success: false };
+		}
+	},
+	updateTodo: async function updateTodo(notion, data) {
+		try {
+			const response = await notion.pages.update({
+				page_id: data.pageID,
+				properties: {
+					Todo: {
+						type: 'title',
+						title: [
+							{
+								text: {
+									content: data.title,
+								},
+							},
+						],
+					},
+					Priority: {
+						select: {
+							name: data.priority,
+						},
+					},
+					Status: {
+						select: {
+							name: data.status,
+						},
+					},
+					Remarks: {
+						rich_text: [
+							{
+								text: {
+									content: data.remarks,
+								},
+							},
+						],
+					},
+				},
+			});
 
-    },
-    createTodo: async function createTodo(notion, databaseID, data) {
-        try {
-            const response = await notion.pages.create({
-                parent: {
-                    database_id: databaseID
-                },
-                properties: {
-                    Todo: {
-                        type: "title",
-                        title: [
-                            {
-                                text: {
-                                    content: data.title
-                                }
-                            }
-                        ]
-                    },
-                    Priority:
-                    {
-                        select: {
-                            name: data.priority
-                        }
-                    },
-                    Status:
-                    {
-                        select: {
-                            name: data.status
-                        }
-                    },
-                    Remarks:
-                    {
-                        rich_text: [
-                            {
-                                text: {
-                                    content: data.remarks
-                                }
-                            }
-                        ]
-                    }
-                }
-            });
-            return { success: true, pageID: response.id }
-        }
-        catch (err) {
-            return { success: false }
-        }
-
-    },
-    deleteTodo: async function deleteTodo(notion, pageID) {
-        try {
-            const response = await notion.blocks.delete({
-                block_id: pageID
-            })
-            console.log(response);
-            return { success: true }
-        }
-        catch (err) {
-            console.log(err);
-            return { success: false }
-        }
-    },
-    updateTodo: async function updateTodo(notion, data) {
-        try {
-            const response = await notion.pages.update({
-                page_id: data.pageID,
-                properties: {
-                    Todo: {
-                        type: "title",
-                        title: [
-                            {
-                                text: {
-                                    content: data.title
-                                }
-                            }
-                        ]
-                    },
-                    Priority:
-                    {
-                        select: {
-                            name: data.priority
-                        }
-                    },
-                    Status:
-                    {
-                        select: {
-                            name: data.status
-                        }
-                    },
-                    Remarks:
-                    {
-                        rich_text: [
-                            {
-                                text: {
-                                    content: data.remarks
-                                }
-                            }
-                        ]
-                    }
-                }
-            })
-
-            return { success: true, pageID: response.id }
-        }
-        catch (err) {
-            return { success: false }
-        }
-    },
-    dev: async function dev(notion, databaseID) {
-        try {
-            const response = await notion.databases.query({
-                database_id: databaseID,
-            });
-            // console.log(response)
-            return { success: true, results: response.results };
-        }
-        catch (err) {
-            return { success: false }
-        }
-    }
+			return { success: true, pageID: response.id };
+		} catch (err) {
+			return { success: false };
+		}
+	},
+	dev: async function dev(notion, databaseID) {
+		try {
+			const response = await notion.databases.query({
+				database_id: databaseID,
+			});
+			return { success: true, results: response.results };
+		} catch (err) {
+			return { success: false };
+		}
+	},
 };
--- server.js
+++ server.js
@@ -1,42 +1,51 @@
-// require("dotenv").config();
-const { readTodo, createTodo, deleteTodo, updateTodo, dev, status } = require("./app");
-const auth = require("./middleware/auth");
-const express = require("express");
+const {
+	readTodo,
+	createTodo,
+	deleteTodo,
+	updateTodo,
+	dev,
+	status,
+} = require('./app');
+const auth = require('./middleware/auth');
+const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-app.use(express.json())
-app.use(auth)
+app.use(express.json());
+app.use(auth);
 
-
-app.get("/", async (req, res) => {
-    const result = await status(res.locals.client, req.query.databaseID);
-    res.send(result);
+app.get('/', async (req, res) => {
+	const result = await status(res.locals.client, req.query.databaseID);
+	res.send(result);
 });
 
 app.post('/createTodo', async (req, res) => {
-    const result = await createTodo(res.locals.client, req.query.databaseID, req.body);
-    res.send(result);
-})
+	const result = await createTodo(
+		res.locals.client,
+		req.query.databaseID,
+		req.body,
+	);
+	res.send(result);
+});
 
-app.get("/readTodo", async (req, res) => {
-    const result = await readTodo(res.locals.client, req.query.databaseID);
-    res.send(result);
+app.get('/readTodo', async (req, res) => {
+	const result = await readTodo(res.locals.client, req.query.databaseID);
+	res.send(result);
 });
 
 app.put('/updateTodo', async (req, res) => {
-    const result = await updateTodo(res.locals.client, req.body);
-    res.send(result);
-})
+	const result = await updateTodo(res.locals.client, req.body);
+	res.send(result);
+});
 
 app.delete('/deleteTodo', async (req, res) => {
-    const result = await deleteTodo(res.locals.client, req.body.pageID);
-    res.send(result);
-})
+	const result = await deleteTodo(res.locals.client, req.body.pageID);
+	res.send(result);
+});
 
 app.get('/dev', async (req, res) => {
-    const result = await dev(res.locals.client, req.query.databaseID);
-    res.send(result);
-})
+	const result = await dev(res.locals.client, req.query.databaseID);
+	res.send(result);
+});
 app.listen(port, () => {
-    console.log(`Server Listening on port ${port}`);
+	console.log(`Server Listening on port ${port}`);
 });
